8b3c2423e004ce36e8c04cc91f20a68e7c699620
Ricardo Oest <oest@bfpi.de>
2017-02-22 06:09:49 +0100

    Datentypen auch entsprechend ändern

--- polygon-in-multipolygon-umwandeln/update-backend-db.sql
+++ polygon-in-multipolygon-umwandeln/update-backend-db.sql
@@ -1,2 +1,2 @@
-UPDATE klarschiff_stadt_grenze SET grenze = ST_Multi(grenze);
-UPDATE klarschiff_stadtteil_grenze SET grenze = ST_Multi(grenze);
+ALTER TABLE klarschiff_stadt_grenze SET DATA TYPE geometry(MultiPolygon, 25833) USING grenze = ST_Multi(grenze);
+ALTER TABLE klarschiff_stadtteil_grenze SET DATA TYPE geometry(MultiPolygon, 25833) USING grenze = ST_Multi(grenze);
