c8f03d33c72ff85f79557bab5cffe77450b0e911
Ricardo Oest <oest@bfpi.de>
2017-02-22 06:16:06 +0100

    Syntax korrigiert

--- polygon-in-multipolygon-umwandeln/update-backend-db.sql
+++ polygon-in-multipolygon-umwandeln/update-backend-db.sql
@@ -1,2 +1,4 @@
-ALTER TABLE klarschiff_stadt_grenze SET DATA TYPE geometry(MultiPolygon, 25833) USING grenze = ST_Multi(grenze);
-ALTER TABLE klarschiff_stadtteil_grenze SET DATA TYPE geometry(MultiPolygon, 25833) USING grenze = ST_Multi(grenze);
+ALTER TABLE klarschiff_stadt_grenze
+  ALTER COLUMN grenze TYPE geometry(MultiPolygon, 25833) USING ST_Multi(grenze);
+ALTER TABLE klarschiff_stadtteil_grenze
+  ALTER COLUMN grenze TYPE geometry(MultiPolygon, 25833) USING ST_Multi(grenze);
